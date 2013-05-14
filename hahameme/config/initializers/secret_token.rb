6ae423a5d87d2f487c8a58eeceda57f7b54a4604
Samir Deeb <samirdeeb@gmail.com>
2013-05-13 22:29:06 -0400

    Fix

--- hahameme/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Hahameme::Application.config.secret_token = 'eb4fdf88a8516e21db481af92086427fe0a10f313194a295c8efdb12d52dc338e2361ebaa5085df0e5c886134df5ee355112bb580733c01d45b4b6ceef304127'
